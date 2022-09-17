56aae1502116479656c66a9ac21f1775dce0900e
Martin Vondrak <vondrak@fhi-berlin.mpg.de>
2022-09-18 00:50:02 +0200

    distance function

---
+++ base.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+float distance(float x1, float x2)
+{
+	return sqrt(pow(x1-x2,2));
+}
+
+float LJ_potential(float x1, float x2, float epsilon, float sigma)
+{
+	return 4*epsilon*(/+/)
+}
+
+int main()
+{
+	cout << distance(3.0,2.0);
+	return 0;
+}
