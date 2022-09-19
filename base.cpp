2adb842ccbf246887d72d96945814ce588484ab0
Martin Vondrak <vondrak@fhi-berlin.mpg.de>
2022-09-19 14:48:54 +0200

    LJ energy

--- base.cpp
+++ base.cpp
@@ -9,14 +9,16 @@ float distance(float x1, float x2)
 	return sqrt(pow(x1-x2,2));
 }
 
-float LJ_potential(float x1, float x2, float epsilon, float sigma)
+float LJ_potential(float dist, float epsilon, float sigma)
 {
-	return 4*epsilon*(/+/)
+	return 4*epsilon*(pow(sigma/dist,12)-pow(sigma/dist,6));
 }
 
 int main()
 {
-	cout << distance(3.0,2.0);
+	for (int i=2; i < 10; i++){
+        cout << LJ_potential(distance(0,i),2.0,3.5) << endl;
+    }
 	return 0;
 }
 
